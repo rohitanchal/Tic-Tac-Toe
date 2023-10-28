07a059c0b3cf24301c66edeeed80f2fc8bea731c
Rohit Kumar Anchal <105239963+rohitanchal@users.noreply.github.com>
2023-10-28 10:22:41 +0530

    complete without logics

---
+++ script.js
@@ -0,0 +1,45 @@
+console.log("Welcome to Tic Tac Toe Game")
+let music = new Audio("music.mp3")
+let audioturn = new Audio("ting.mp3")
+let gameover = new Audio("gameover.mp3")
+let turn = "X";
+
+// Function to change the turn
+const changeTurn = () => {
+    return turn === "X" ? "0" : "X"
+}
+
+// Function to check for a Win
+
+const checkWin = () => {
+    // let boxtexts = document.getElementsByClassName('boxtext')
+    // let wins = [
+    //     [0, 1, 2],
+    //     [3, 4, 5],
+    //     [6, 7, 8],
+    //     [0, 3, 6],
+    //     [1, 4, 7],
+    //     [2, 5, 8],
+    //     [0, 4, 8],
+    //     [2, 4, 6],
+    // ]
+    // wins.forEach(e => {
+
+    // })
+}
+
+// Game Logics Start
+
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element => {
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click', () => {
+        if (boxtext.innerText === '') {
+            boxtext.innerText = turn;
+            turn = changeTurn();
+            audioturn.play();
+            checkWin();
+            document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+        }
+    })
+})
