c356315be0f0d149ec8a03d150bca36bdc0f1835
Rohit Kumar Anchal <105239963+rohitanchal@users.noreply.github.com>
2023-10-28 10:45:39 +0530

    add won logics

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let music = new Audio("music.mp3")
 let audioturn = new Audio("ting.mp3")
 let gameover = new Audio("gameover.mp3")
 let turn = "X";
+// let isgameover = false
 
 // Function to change the turn
 const changeTurn = () => {
@@ -12,20 +13,22 @@ const changeTurn = () => {
 // Function to check for a Win
 
 const checkWin = () => {
-    // let boxtexts = document.getElementsByClassName('boxtext')
-    // let wins = [
-    //     [0, 1, 2],
-    //     [3, 4, 5],
-    //     [6, 7, 8],
-    //     [0, 3, 6],
-    //     [1, 4, 7],
-    //     [2, 5, 8],
-    //     [0, 4, 8],
-    //     [2, 4, 6],
-    // ]
-    // wins.forEach(e => {
-
-    // })
+    let boxtexts = document.getElementsByClassName('boxtext')
+    let wins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    wins.forEach(e => {
+        if ((boxtexts[e[0]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[2]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[0]].innerText !== "")) {
+            document.querySelector('.info').innerText = boxtexts[e[0]].innerText + "won"
+        }
+    })
 }
 
 // Game Logics Start
